6262af90e9e70c6e74bfffd76615963e487a86af
rupelio.c.junior <rupelio.c.junior@exxonmobil.com>
2023-03-20 14:05:31 -0300

    primeiro

--- ssg/extensions.py
+++ ssg/extensions.py
@@ -3,5 +3,6 @@ import importlib
 from pathlib import Path
 
 def load_module(directory, name):
-    sys.path.insert().directory[0]
-    import_module(name)
+    sys.path.insert(directory[0])
+    importlib.import_module(name)
+    sys.path.pop(name[0])
