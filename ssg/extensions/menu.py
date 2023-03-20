c5fced950aa68dc67d13ff3d3918373a97596181
rupelio.c.junior <rupelio.c.junior@exxonmobil.com>
2023-03-20 15:16:16 -0300

    primeiro

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -2,6 +2,10 @@ from ssg import hooks, parsers
 
 files = []
 
+@hooks.register("collect_files")
 def collect_files(source, site_parsers):
-    hooks.register("collect_files")
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
+    for path in source.rglob("*"):
+        for parser in list(filter(valid, site_parsers)):
+            if parsers.valid_file_ext(path.suffix):
+                path.append(files)
