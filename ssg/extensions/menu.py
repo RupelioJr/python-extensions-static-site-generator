1020b5822d7085f83985a666bf2e8d549e29b45a
rupelio.c.junior <rupelio.c.junior@exxonmobil.com>
2023-03-20 14:41:23 -0300

    primeiro

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -4,4 +4,4 @@ files = []
 
 def collect_files(source, site_parsers):
     hooks.register("collect_files")
-    valid = lambda(p, isinstance(p, parsers.ResourceParser))
+    valid = lambda p: not isinstance(p, parsers.ResourceParser)
