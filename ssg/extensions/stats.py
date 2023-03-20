466b319d908e7d1a6f88415ec28f403c5d4c2020
rupelio.c.junior <rupelio.c.junior@exxonmobil.com>
2023-03-20 15:40:25 -0300

    primeiro

--- ssg/extensions/stats.py
+++ ssg/extensions/stats.py
@@ -4,6 +4,7 @@ import times
 start_time = None
 total_written = 0
 
-@start_build
-def start_build:
-    global start_time = times
+@hooks.register("start_build")
+def start_build():
+    global start_time
+    start_time = times.ctime
