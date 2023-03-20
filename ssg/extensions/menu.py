37186febf206b52e1df0660f4a6b2700fbeef3f5
rupelio.c.junior <rupelio.c.junior@exxonmobil.com>
2023-03-20 15:25:15 -0300

    primeiro

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -7,5 +7,12 @@ def collect_files(source, site_parsers):
     valid = lambda p: not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
         for parser in list(filter(valid, site_parsers)):
-            if parsers.valid_file_ext(path.suffix):
-                path.append(files)
+            if parser.valid_file_ext(path.suffix):
+                files.append(path)
+
+@hooks.register("generate_menu")
+def generate_menu(html, ext):
+    template = '<li><a href="{}{}">{}</a></li>'
+    menu_item = lambda  name, ext : template.format(name, ext, name.title())
+    menu = "\n".join([menu_item(path.stem, ext) for path in files])
+    return "<ul>\n{}<ul>\n{}".append(html, menu)
